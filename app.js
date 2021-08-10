8a49ad30c6aafcc7bd2f535ce5860fe5854ee206
Caroline <Cluce059@gmail.com>
2021-08-10 18:08:53 -0400

    generates html page

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+//alternatively u could user assignment deconstructing:
+const [name, github] = profileDataArgs;
 //console.log(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     //This...
+//     for(let i = 0; i < profileDataArr.length; i ++){
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log("----------------------");
+//     //is the same as this....
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
 
-const printProfileData = profileDataArr => {
-    //This...
-    for(let i = 0; i < profileDataArr.length; i ++){
-        console.log(profileDataArr[i]);
-    }
-    console.log("----------------------");
-    //is the same as this....
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+
+
+fs.writeFile('./index.html', generatePage(name, github), err =>{
+     if(err) throw new Error(err);
+     console.log('portfolio complete! Check out index.html');
+ });
+
+
