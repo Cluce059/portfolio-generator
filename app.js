3bf6c1b8d8a7fc7c6431e9b3af2dc0874a2f0c52
Caroline <Cluce059@gmail.com>
2021-08-10 19:40:18 -0400

    inquirer prompt

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-//const name = profileDataArgs[0];
-//const github = profileDataArgs[1];
-//alternatively u could user assignment deconstructing:
-const [name, github] = profileDataArgs;
-//console.log(profileDataArgs);
-// const printProfileData = profileDataArr => {
-//     //This...
-//     for(let i = 0; i < profileDataArr.length; i ++){
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log("----------------------");
-//     //is the same as this....
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+const inquirer = require('inquirer');
 
-// printProfileData(profileDataArgs);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+// //const name = profileDataArgs[0];
+// //const github = profileDataArgs[1];
+// //alternatively u could user assignment deconstructing:
+// const pageHTML = generatePage(name, github);
 
-
-
-fs.writeFile('./index.html', generatePage(name, github), err =>{
-     if(err) throw new Error(err);
-     console.log('portfolio complete! Check out index.html');
- });
-
+// fs.writeFile('./index.html', pageHTML, err =>{
+//      if(err) throw err;
+//      console.log('portfolio complete! Check out index.html');
+// });
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
